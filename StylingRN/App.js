9f7d99177b1ce4a069768c071b443179366bbe1c
Manoj <mmmanojsharma201611@gmail.com>
2023-09-06 18:41:39 +0530

    Apply multiple css to component

--- StylingRN/App.js
+++ StylingRN/App.js
@@ -1,21 +1,36 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>React Native Stylesheet Api</Text>
+
+      <View style={[styles.lightBlueBg,styles.box]}>Light Blue Box</View>
+      <View style={[styles.lightGreenBg,styles.box]}>Light Green Box</View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
-    backgroundColor:"plum",
-    padding:60
+    flex: 1,
+    backgroundColor: "plum",
+    padding: 60,
   },
-  title:{
-    backgroundColor:"red",
-    color:"white",
-  }
+  title: {
+    backgroundColor: "red",
+    color: "white",
+  },
+  box: {
+    width: 100,
+    height: 100,
+    padding: 10,
+    backgroundColor: "white", // property at last take presedence
+  },
+  lightBlueBg: {
+    backgroundColor: "lightblue",
+  },
+  lightGreenBg: {
+    backgroundColor: "lightgreen",
+  },
 });

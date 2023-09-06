432e76d523e0f71f81bb7a5df4a4fa9147b46ae8
Manoj <mmmanojsharma201611@gmail.com>
2023-09-06 17:54:16 +0530

    Added custom component

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,9 +1,11 @@
 import { View, Alert, Button } from "react-native";
-
+import Greet from "./components/Greet";
 const logoImage = require("./assets/adaptive-icon.png");
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
+    <Greet name="Manoj"/>
+    <Greet name="Mj"/>
     </View>
   );
 }

5c4fc8579bead30065537f60f99574621fc6e4a2
Manoj <mmmanojsharma201611@gmail.com>
2023-09-08 10:12:53 +0530

    Box Styling (margin,padding,border,borderRadius)

--- StylingRN/App.js
+++ StylingRN/App.js
@@ -5,8 +5,8 @@ export default function App() {
     <View style={styles.container}>
       <Text style={styles.title}>React Native Stylesheet Api</Text>
 
-      <View style={[styles.lightBlueBg,styles.box]}>Light Blue Box</View>
-      <View style={[styles.lightGreenBg,styles.box]}>Light Green Box</View>
+      <View style={[styles.lightBlueBg, styles.box]}>Light Blue Box</View>
+      <View style={[styles.lightGreenBg, styles.box]}>Light Green Box</View>
     </View>
   );
 }
@@ -24,8 +24,17 @@ const styles = StyleSheet.create({
   box: {
     width: 100,
     height: 100,
-    padding: 10,
-    backgroundColor: "white", // property at last take presedence
+    // padding: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 20,
+    //margin:10
+    marginVertical: 10,
+    // border
+    borderWidth: 2,
+    borderColor: "white",
+    borderStyle: "solid",
+    borderRadius: "5",
+    // backgroundColor: "white", // property at last take presedence
   },
   lightBlueBg: {
     backgroundColor: "lightblue",
@@ -34,3 +43,6 @@ const styles = StyleSheet.create({
     backgroundColor: "lightgreen",
   },
 });
+
+// Note: Border Radius in View is Application on Adnroid and IOS,
+//  while borderRadius in Text only apply on Android
